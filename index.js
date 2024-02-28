c501b89a8e7a0d90fcfe0443f316f635e8a3d2e7
Felipe Jerez <piperock2000@gmail.com>
2024-02-28 16:27:00 -0300

    add funtion showModal, New Project button

---
+++ index.js
@@ -0,0 +1,13 @@
+function showModal(){
+    const modal = document.getElementById("new-project-modal")
+    modal.showModal()
+}
+
+// const showModal = () => {
+//     const modal = document.getElementById("new-project-modal")
+//     modal.showModal()
+// }
+
+
+const newProjectBtn = document.getElementById("new-project-btn")
+newProjectBtn.addEventListener("click", showModal)
