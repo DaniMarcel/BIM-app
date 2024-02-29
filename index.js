9ed77d11223c110f761b969cf49b0a5659f58b74
Felipe Jerez Abarca <plague@Felipes-MacBook-Pro.local>
2024-02-29 20:36:18 -0300

    funtion and ids for the form

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-function showModal(){
-    const modal = document.getElementById("new-project-modal")
-    modal.showModal()
+function showModal(id){
+    const modal = document.getElementById(id)
+    if(modal){
+        modal.showModal()
+    }else{
+        console.warn("The provided modal wasn't found. ID: ", id)
+    }
 }
 
-// const showModal = () => {
-//     const modal = document.getElementById("new-project-modal")
-//     modal.showModal()
-// }
-
-
 const newProjectBtn = document.getElementById("new-project-btn")
-newProjectBtn.addEventListener("click", showModal)
+if(newProjectBtn){
+    newProjectBtn.addEventListener("click", () => {showModal("new-project-modal")})
+}else{
+    console.warn("New projects button was not found")
+}
+
+const projectForm = document.getElementById("new-project-form")
+if (projectForm) {
+    projectForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const formData = new FormData(projectForm)
+    console.log("Description:", formData.get("description"))
+    })
+} else {
+    console.warn("The project form was not found. Check the ID!")
+}
