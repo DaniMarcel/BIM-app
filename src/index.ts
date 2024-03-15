a9ce8992a398213a11a6e87ed0207512e09eb824
Felipe Jerez Abarca <plague@Felipes-MacBook-Pro.local>
2024-03-15 14:21:31 -0300

    add logic export and import

--- src/index.ts
+++ src/index.ts
@@ -54,3 +54,17 @@ if (projectForm && projectForm instanceof HTMLFormElement) {
 }else{
     console.warn("The project form was not found. Check the ID!")
 }
+
+const exportProjectsBtn= document.getElementById("export-projects-btn")
+if (exportProjectsBtn) {
+    exportProjectsBtn.addEventListener("click", () => {
+        projectsManager.exportToJSON()
+    })
+}
+
+const importProjectsBtn = document.getElementById("import-projects-btn")
+if (importProjectsBtn) {
+    importProjectsBtn.addEventListener("click", () => {
+        projectsManager.importFromJSON()
+    })
+}
