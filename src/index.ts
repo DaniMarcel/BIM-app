3f6c15ae5d85bf1d61fb02812bb4b4dd635b2fd6
Felipe Jerez <piperock2000@gmail.com>
2024-06-17 05:37:58 -0400

    Importar modelos IFC, ver elementos del modelo, seleccinar objeto con sus propiedades

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import * as THREE from "three"
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls" 
+import * as OBC from "openbim-components"
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader"
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader"
 import { IProject, ProjectStatus, UserRole } from "./class/Project"
 import { ProjectsManager } from "./class/ProjectsManager"
 //
@@ -56,14 +60,14 @@ if (projectForm && projectForm instanceof HTMLFormElement) {
 }else{
     console.warn("The project form was not found. Check the ID!")
 }
-
+//Export to JSON
 const exportProjectsBtn= document.getElementById("export-projects-btn")
 if (exportProjectsBtn) {
     exportProjectsBtn.addEventListener("click", () => {
         projectsManager.exportToJSON()
     })
 }
-
+// Import from JSON
 const importProjectsBtn = document.getElementById("import-projects-btn")
 if (importProjectsBtn) {
     importProjectsBtn.addEventListener("click", () => {
@@ -71,35 +75,109 @@ if (importProjectsBtn) {
     })
 }
 
-// ThreeJS Viewer
-const scene = new THREE.Scene()
-
-const viewerContainer = document.getElementById("viewer-container") as HTMLElement
-const containerDimensions = viewerContainer.getBoundingClientRect()
-const aspectRatio = containerDimensions.width / containerDimensions.height
-const camera = new THREE.PerspectiveCamera(75, aspectRatio)
-camera.position.z = 5
-
-const renderer = new THREE.WebGLRenderer()
-viewerContainer.append(renderer.domElement)
-renderer.setSize(containerDimensions.width, containerDimensions.height)
 
 
-const BoxGeometry = new THREE.BoxGeometry()
-const material = new THREE.MeshStandardMaterial()
-const cube = new THREE.Mesh(BoxGeometry, material)
 
-const directionalLight = new THREE.DirectionalLight()
-const ambientLight = new THREE.AmbientLight()
-ambientLight.intensity = 0.4
+// OpenBIM-Components viewer
+// Set up Scene
+const viewer = new OBC.Components()
 
-scene.add(cube, directionalLight, ambientLight)
+const sceneComponent = new OBC.SimpleScene(viewer)
+sceneComponent.setup()
+viewer.scene = sceneComponent
+// Obtiene la escena de THREE.Scene
+const scene = sceneComponent.get()
+scene.background = null
 
-const cameraControls = new OrbitControls(camera, viewerContainer)
+// Componente renderizador
+const viewerContainer = document.getElementById("viewer-container") as HTMLDivElement
+const rendererComponent = new OBC.PostproductionRenderer(viewer, viewerContainer)
+viewer.renderer = rendererComponent
 
-function renderScene(){
-    renderer.render(scene, camera)
-    requestAnimationFrame(renderScene)
+// Camera
+const cameraComponent = new OBC.OrthoPerspectiveCamera(viewer)
+viewer.camera = cameraComponent
+// ray selector
+const raycasterComponent = new OBC.SimpleRaycaster(viewer)
+viewer.raycaster = raycasterComponent
+
+//inicializacion, correction camera, sombra
+viewer.init()
+cameraComponent.updateAspect()
+rendererComponent.postproduction.enabled = true
+
+const ifcLoader = new OBC.FragmentIfcLoader(viewer)
+// ruta absoluta biblioteca WebIFC
+ifcLoader.settings.wasm = {
+    path: "https://unpkg.com/web-ifc@0.0.43/",
+    absolute: true
+}
+// Light resaltar
+const highlighter = new OBC.FragmentHighlighter(viewer)
+highlighter.setup()
+
+const propertiesProcessor = new OBC.IfcPropertiesProcessor(viewer)
+// procesador de propiedades limpia las lista de propiedades
+highlighter.events.select.onClear.add(() => {
+    propertiesProcessor.cleanPropertiesList()
+})
+
+// Clasificar modelos en grupos
+const classifier = new OBC.FragmentClassifier(viewer)
+// Ventana de propiedades
+const classificationWindow = new OBC.FloatingWindow(viewer)
+classificationWindow.visible = false
+viewer.ui.add(classificationWindow)
+classificationWindow.title = "Grupo de Modelos"
+
+// boton ocultar window
+const classificationsBtn = new OBC.Button(viewer)
+classificationsBtn.materialIcon = "account_tree"
+
+classificationsBtn.onClick.add(() => {
+    classificationWindow.visible = !classificationWindow.visible
+    classificationsBtn.active = classificationWindow.visible
+})
+
+async function createModelTree(){
+    // grupo de modelos para la ventana storeys=pisos entities=entidades
+    const fragmentTree = new OBC.FragmentTree(viewer)
+    await fragmentTree.init()
+    await fragmentTree.update(["storeys", "entities"])
+    fragmentTree.onHovered.add((fragmentMap) => {
+        highlighter.highlightByID("hover", fragmentMap)
+    })
+    fragmentTree.onSelected.add((fragmentMap) =>{
+        highlighter.highlightByID("select", fragmentMap)
+    })
+    const tree = fragmentTree.get().uiElement.get("tree")
+    return tree
 }
 
-renderScene()
+ifcLoader.onIfcLoaded.add(async (model) => {
+    // highlighter resaltador elemnto seleccionado
+    highlighter.update()
+    // Claseficar modelos en grupos
+    classifier.byStorey(model)
+    classifier.byEntity(model)
+    const tree = await createModelTree()
+    // Agrupar las propiedades de los modelos
+    await classificationWindow.slots.content.dispose(true)
+    classificationWindow.addChild(tree)
+
+    // Propiedades de modelo seleccionado a traves de expresID con highlighter
+    propertiesProcessor.process(model)
+    highlighter.events.select.onHighlight.add((fragmentMap) =>{
+        const expressID = [...Object.values(fragmentMap)[0]][0]
+        propertiesProcessor.renderProperties(model, Number(expressID))
+    })
+})
+
+// Tools bar
+const toolbar = new OBC.Toolbar(viewer)
+toolbar.addChild(
+    ifcLoader.uiElement.get("main"),
+    classificationsBtn,
+    propertiesProcessor.uiElement.get("main")
+)
+viewer.ui.addToolbar(toolbar)
