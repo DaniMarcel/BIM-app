0359542a6f466d974f3f520c535d2160ad10ff13
Felipe Jerez <piperock2000@gmail.com>
2024-06-28 19:51:32 -0400

    add connection with firebase-app

---
+++ src/firebase/index.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCI6X9-xQ7RE1dQXgxGyh1gcSTqU0-_vFs",
+  authDomain: "bim-dev-app-e0acb.firebaseapp.com",
+  projectId: "bim-dev-app-e0acb",
+  storageBucket: "bim-dev-app-e0acb.appspot.com",
+  messagingSenderId: "962220324993",
+  appId: "1:962220324993:web:5fa317a24911a6c639b580"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
